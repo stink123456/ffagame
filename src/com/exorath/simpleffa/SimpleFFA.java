3382c2730d95762ecad3c15d7bfa3bd1f768a1ce
Toon Sevrin <twan123@live.be>
2015-09-01 22:51:36 +0200

    Added my stuff to gitignore

--- src/com/exorath/simpleffa/SimpleFFA.java
+++ src/com/exorath/simpleffa/SimpleFFA.java
@@ -1,5 +1,6 @@
 package com.exorath.simpleffa;
 
+import com.exorath.game.GameAPI;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.exorath.game.api.Game;
@@ -8,11 +9,12 @@ import com.exorath.game.api.GameProvider;
 /**
  * Created by TOON on 8/23/2015.
  */
-public class SimpleFFA extends JavaPlugin implements GameProvider {
-
+    public class SimpleFFA extends JavaPlugin implements GameProvider {
+    //This method needs to be called lol
     @Override
     public Game create() {
-        return new FFAGame();
+        Game game = new FFAGame();
+        return game;
     }
 
 }
